c0725d2432699bf6ad69650add42f76e8f98073a
Niklas 'Aberen' Horn <nijoergensen@gmail.com>
2019-04-21 01:12:44 +0200

    refactor(changelog): Removed changelog script, and added package.json solution

--- generate-changelog.js
+++
@@ -1,7 +0,0 @@
-var Changelog = require('generate-changelog');
-var Fs        = require('fs');
-
-return Changelog.generate({ patch: true, repoUrl: 'https://github.com/formaal/principles' })
-.then(function (changelog) {
-  Fs.writeFileSync('./CHANGELOG.md', changelog);
-});
